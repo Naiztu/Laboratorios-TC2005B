866aec924be13d1e7ab571acdd55db2aab5d1dec
Naiztu <A01707404@tec.mx>
2022-03-01 22:21:11 -0600

    fixed environment variable

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    USER: process.env.USER,
+    PASS: process.env.PASS,
+  },
 }
 
 module.exports = nextConfig
