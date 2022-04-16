07a259df902cc11adfb6f9280b1658d731bdff85
Naiztu <A01707404@tec.mx>
2022-04-16 12:28:56 -0500

    add lab 15

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Layaout from "../components/Layaout";
 import LinkLaboratorios from "../components/LinkLaboratorios";
 import { FaArrowDown } from "react-icons/fa";
 
-const labs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 17];
+const labs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17];
 
 export default function Home() {
   return (
