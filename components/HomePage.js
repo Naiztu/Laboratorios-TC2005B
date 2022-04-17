97d3e331fd0d85f08f9eb2c8b97faeaa8a3a622f
Naiztu <A01707404@tec.mx>
2022-04-16 20:38:43 -0500

    fixed deploy

--- components/HomePage.js
+++ components/HomePage.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 export default function HomePage({ user: { user, setUser } }) {
     return (
-        <div class="hero min-h-screen bg-base-200">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src="/img/hed.gif" class=" w-11/12 mx-auto max-w-sm rounded-lg shadow-2xl" />
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src="/img/hed.gif" className=" w-11/12 mx-auto max-w-sm rounded-lg shadow-2xl" />
                 <div>
-                    <h1 class="text-5xl font-bold">Bienvenido</h1>
-                    <p class="py-6 text-neutral">Solo los que estan registrados en la base de datos pueden entrar a esta pagina.<br /> Como tu: {user.username}</p>
-                    <button class="btn " onClick={() => setUser(null)}>Salir</button>
+                    <h1 className="text-5xl font-bold">Bienvenido</h1>
+                    <p className="py-6 text-neutral">Solo los que estan registrados en la base de datos pueden entrar a esta pagina.<br /> Como tu: {user.username}</p>
+                    <button className="btn " onClick={() => setUser(null)}>Salir</button>
                 </div>
             </div>
         </div>
